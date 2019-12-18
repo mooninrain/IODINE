77790f41183977f5bb0176e30686d11ae19dfad6
Ruidong Wu <mooninrain@outlook.com>
2019-12-18 13:57:27 -0500

    Update clevr3.py

--- lib/data/clevr3.py
+++ lib/data/clevr3.py
@@ -5,6 +5,7 @@ import os
 import numpy as np
 import torch
 from PIL import Image, ImageFile
+
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 class CLEVR3(Dataset):
@@ -19,6 +20,14 @@ class CLEVR3(Dataset):
             self.img_paths.append(img_path)
             
         self.img_paths.sort()
+
+        self.masks = []
+        scene_path = os.path.join(root, 'scenes.json')
+        with open(scene_path,'r') as r:
+            scene_data = json.load(r)['scenes']
+        for k,scene in enumerate(scene_data):
+            assert scene['image_filename'] == os.path.split(img_paths[k])[-1]
+            self.masks.append(annotate_masks(scene))
         
     def __getitem__(self, index):
         img_path = self.img_paths[index]
@@ -34,49 +43,36 @@ class CLEVR3(Dataset):
             transforms.ToPILImage(),
             transforms.Resize(128, interpolation=Image.NEAREST),
         ])
-        filename = os.path.split(img_path)[-1]
-        mask_path = os.path.join(self.root, 'masks', filename)
-        mask = None
-        if os.path.exists(mask_path):
-            mask = io.imread(mask_path)
-            mask = self.sep(mask)
-            mask = [np.array(mask_transform(x[:, :, None].astype(np.uint8))) for x in mask]
-            mask = np.stack(mask, axis=0)
-            mask = torch.from_numpy(mask.astype(np.float)).float()
-        # mask = None
+        
+        mask = self.masks[index]
+        mask = [np.array(mask_transform(x[:, :, None].astype(np.uint8))) for x in mask]
+        mask = np.stack(mask, axis=0)
+        mask = torch.from_numpy(mask.astype(np.float)).float()
         
         return img, mask
         
     def __len__(self):
         return len(self.img_paths)
 
-    def sep(self, img):
-        """
-        Seperate a color image into masks
-        :img: (H, W, 3)
-        :return: (K, H, W), bool array
-        """
-        img = img[:, :, :3]
-        # a = set()
-        # for i in range(img.shape[0]):
-        #     for j in range(img.shape[1]):
-        #         pixel = tuple(img[i, j][:3])
-        #         if pixel not in a and pixel != (64, 64, 64):
-        #             a.add(pixel)
-        H, W, _ = img.shape
-        pixels = list(tuple(pix) for pix in img.reshape(H * W, 3))
-        a = set(pixels)
-        # background
-        a.remove((64, 64, 64))
-        masks = []
-        for pixel in a:
-            pixel = np.array(pixel)
-            # (h, w, 3)
-            mask = img == pixel
-            mask = np.all(mask, 2)
-            masks.append(mask)
-    
-        return masks
-    
-    
-    
+import jaclearn.vision.coco.mask_utils as mask_utils
+
+def _is_object_annotation_available(scene):
+    if len(scene['objects']) > 0 and 'mask' in scene['objects'][0]:
+        return True
+    return False
+
+def _get_object_masks(scene):
+    """Backward compatibility: in self-generated clevr scenes, the groundtruth masks are provided;
+    while in the clevr test data, we use Mask R-CNN to detect all the masks, and stored in `objects_detection`."""
+    if 'objects_detection' not in scene:
+        return scene['objects']
+    if _is_object_annotation_available(scene):
+        return scene['objects']
+    return scene['objects_detection']
+
+def annotate_masks(scene):
+    if 'objects' not in scene and 'objects_detection' not in scene:
+        return list()
+
+    masks = [mask_utils.decode(i['mask']).astype('float32') for i in _get_object_masks(scene)]
+    return masks
